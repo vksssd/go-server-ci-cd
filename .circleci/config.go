9a5c3094e6486b61a0a956eeb5618542d8ee67c8
vksssdks <2820142.cse@pietgroup.co.in>
2024-06-05 20:27:20 +0530

    inint

--- .circleci/config.go
+++ .circleci/config.go
@@ -1,43 +1,43 @@
-// version: 2.1
-
-// jobs:
-//   build:
-//     docker:
-//       - image: circleci/golang:1.16
-//     working_directory: /go/src/github.com/vksssd/go-server-ci-cd
-//     steps:
-//       - checkout
-//       - run: go mod download
-//       - run: go test ./...
-//       - run: go build -o server .
-
-// workflows:
-//   version: 2
-//   build:
-//     jobs:
-//       - build
-
 version: 2.1
 
 jobs:
   build:
     docker:
       - image: circleci/golang:1.16
-    working_directory: /go/src/github.com/<your_username>/go-server-ci-cd
+    working_directory: /go/src/github.com/vksssd/go-server-ci-cd
     steps:
       - checkout
       - run: go mod download
       - run: go test ./...
       - run: go build -o server .
-      - run:
-          name: Build and push Docker image
-          command: |
-            echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-            docker build -t <your_dockerhub_username>/go-server-ci-cd .
-            docker push <your_dockerhub_username>/go-server-ci-cd
 
 workflows:
   version: 2
   build:
     jobs:
       - build
+
+// version: 2.1
+
+// jobs:
+//   build:
+//     docker:
+//       - image: circleci/golang:1.16
+//     working_directory: /go/src/github.com/<your_username>/go-server-ci-cd
+//     steps:
+//       - checkout
+//       - run: go mod download
+//       - run: go test ./...
+//       - run: go build -o server .
+//       - run:
+//           name: Build and push Docker image
+//           command: |
+//             echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+//             docker build -t <your_dockerhub_username>/go-server-ci-cd .
+//             docker push <your_dockerhub_username>/go-server-ci-cd
+
+// workflows:
+//   version: 2
+//   build:
+//     jobs:
+//       - build
