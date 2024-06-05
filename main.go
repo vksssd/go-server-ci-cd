7ac255a99969719b870f90c3ec2e459445869d5b
vksssdks <2820142.cse@pietgroup.co.in>
2024-06-05 21:08:38 +0530

    fixing watch tower

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, this is the Go server by vinayak singh!")
+    fmt.Fprintf(w, "Hello, this is the Go server!")
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -23,7 +23,7 @@ func TestHandler(t *testing.T) {
             status, http.StatusOK)
     }
 
-    expected := "Hello, this is the Go server by vinayak singh!"
+    expected := "Hello, this is the Go server!"
     if rr.Body.String() != expected {
         t.Errorf("handler returned unexpected body: got %v want %v",
             rr.Body.String(), expected)
